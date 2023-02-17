6791b804c63c364854f8278b5aa504c06dd4fb78
mamadou <deakamm2@gmail.com>
2023-02-17 15:06:34 +0100

    change the route

--- routes/web.php
+++ routes/web.php
@@ -2,48 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmpleadoController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+Auth::routes(['register'=>false,'reset'=>false]);//autenticación por seguridad
 
 Route::get('/', function () {
     return view('auth.login');
 });
 
-// Route::get('/empleado', function () {
-//     return view('empleado.index');
-// });
-// Route::get('empleado/create',[EmpleadoController::class,'create']);
+Route::resource('empleado', EmpleadoController::class)->middleware('auth');
 
-
-Route::resource('empleado',EmpleadoController::class)->middleware('auth');//authentificación du login
-Auth::routes(['register'=>false,'reset'=>false]);//authentifacación por la securidad 
-
-Route::get('/home', [EmpleadoController::class, 'index'])->name('home');
-
-Route::group(['middleware'=>'auth'], function(){
-    
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/', [EmpleadoController::class, 'index'])->name('home');
-
 });
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
